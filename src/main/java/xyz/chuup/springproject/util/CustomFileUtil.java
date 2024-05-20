c8b02102752d80c38d779cf6be3ef2d74fa151cf
fxzz <fxzzy@naver.com>
2024-05-20 21:57:06 +0900

    thumbnail 추가

--- src/main/java/xyz/chuup/springproject/util/CustomFileUtil.java
+++ src/main/java/xyz/chuup/springproject/util/CustomFileUtil.java
@@ -3,6 +3,7 @@ package xyz.chuup.springproject.util;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -47,10 +48,23 @@ public class CustomFileUtil {
 
             String savedName = UUID.randomUUID().toString()+"_"+file.getOriginalFilename();
 
+
+
             Path savePath = Paths.get(uploadPath, savedName);
 
             try {
                 Files.copy(file.getInputStream(), savePath);
+
+                String contentType = file.getContentType(); // Mime type
+
+                if (contentType != null && contentType.startsWith("image")) {
+
+                    Path thumbnailPath = Paths.get(uploadPath, "s_"+savedName);
+                    Thumbnails.of(savePath.toFile())
+                            .size(200,200)
+                            .toFile(thumbnailPath.toFile());
+                }
+
                 uploadNames.add(savedName);
             } catch (IOException e) {
                 throw new RuntimeException(e);
